b2d2a1c3ee17c26235b8e7a5c712a9688add42ad
Hferpi <hferpi@gmail.com>
2025-09-03 18:54:43 +0200

    mejora header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -14,16 +14,16 @@ export default function Header() {
       }`}>
       
       <div className="flex justify-between items-center px-8 max-w-7xl mx-auto">
-        <div className="flex items-center gap-4">
-      <img src="icono.jpeg" alt="icono" className="w-11 h-11 md:w-14 md:h-14 rounded-xl" />
+        <div className="flex items-center md:gap-4 gap-2">
+      <img src="icono.jpeg" alt="icono" className="w-10 h-10 md:w-14 md:h-14 rounded-xl" />
         <h1 className={`underline font-bold transition-all duration-500 ${
-          isScrolled ? 'text-xl text-gray-800' : 'text-2xl text-white drop-shadow-lg'
+          isScrolled ? 'md:text-xl text-gray-800' : 'text-2xl text-white drop-shadow-lg'
         }`}>
           
           EL TENTADERO
         </h1>
         </div>
-        <nav className="space-x-6">
+        <nav className="md:space-x-6 space-x-3">
         <a 
             href="#reseñas" 
             className={`font-medium transition-all duration-300 hover:scale-110 ${
